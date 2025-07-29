ea5b1738e9eee37030d08267a9cd6f50694330bf
Racheal Adeyemo <hrmomotoke@gmail.com>
2025-07-29 20:21:56 +0100

    fix: set correct port and startup config for azure

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home () :
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return "Hello from Ray on Azure!"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080)) # Default to 8080
+    app.run(host='0.0.0.0', port=port)
